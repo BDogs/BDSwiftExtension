23c4896f181d0f648d643381c078c2e78380f5ef
宋志 <songzhi@pinsmedical.com>
2018-09-04 16:18:37 +0800

    Float extensin

---
+++ Classes/Swift/Float/Float+Normal.swift
@@ -0,0 +1,152 @@
+//
+//  Float+Normal.swift
+//  Example
+//
+//  Created by 诸葛游 on 2018/9/4.
+//  Copyright © 2018年 品驰医疗. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Properties
+public extension FloatingPoint {
+    
+    /// Absolute value of integer number.
+    public var abs: Self {
+        return Swift.abs(self)
+    }
+    
+    /// Check if integer is positive.
+    public var isPositive: Bool {
+        return self > 0
+    }
+    
+    /// Check if integer is negative.
+    public var isNegative: Bool {
+        return self < 0
+    }
+    
+    /// Ceil of number.
+    public var ceil: Self {
+        return Foundation.ceil(self)
+    }
+    
+    /// Radian value of degree input.
+    public var degreesToRadians: Self {
+        return Self.pi * self / Self(180)
+    }
+    
+    /// Floor of number.
+    public var floor: Self {
+        return Foundation.floor(self)
+    }
+    
+    /// Degree value of radian input.
+    public var radiansToDegrees: Self {
+        return self * Self(180) / Self.pi
+    }
+    
+}
+
+// MARK: - Methods
+public extension FloatingPoint {
+    
+    /// Random number between two number.
+    ///
+    /// - Parameters:
+    ///   - min: minimum number to start random from.
+    ///   - max: maximum number random number end before.
+    /// - Returns: random number between two numbers.
+    public static func random(between min: Self, and max: Self) -> Self {
+        let aMin = Self.minimum(min, max)
+        let aMax = Self.maximum(min, max)
+        let delta = aMax - aMin
+        return Self(arc4random()) / Self(UInt64(UINT32_MAX)) * delta + aMin
+    }
+    
+    /// Random number in a closed interval range.
+    ///
+    /// - Parameter range: closed interval range.
+    /// - Returns: random number in the given closed range.
+    public static func random(inRange range: ClosedRange<Self>) -> Self {
+        let delta = range.upperBound - range.lowerBound
+        return Self(arc4random()) / Self(UInt64(UINT32_MAX)) * delta + range.lowerBound
+    }
+    
+}
+
+// MARK: - Initializers
+public extension FloatingPoint {
+    
+    /// Created a random number between two numbers.
+    ///
+    /// - Parameters:
+    ///   - min: minimum number to start random from.
+    ///   - max: maximum number random number end before.
+    public init(randomBetween min: Self, and max: Self) {
+        let aMin = Self.minimum(min, max)
+        let aMax = Self.maximum(min, max)
+        let delta = aMax - aMin
+        self = Self(arc4random()) / Self(UInt64(UINT32_MAX)) * delta + aMin
+    }
+    
+    /// Create a random number in a closed interval range.
+    ///
+    /// - Parameter range: closed interval range.
+    public init(randomInRange range: ClosedRange<Self>) {
+        let delta = range.upperBound - range.lowerBound
+        self = Self(arc4random()) / Self(UInt64(UINT32_MAX)) * delta + range.lowerBound
+    }
+    
+}
+
+
+// MARK: - Operators
+
+precedencegroup PowerPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ** : PowerPrecedence
+/// Value of exponentiation.
+///
+/// - Parameters:
+///   - lhs: base float.
+///   - rhs: exponent float.
+/// - Returns: exponentiation result (4.4 ** 0.5 = 2.0976176963).
+public func ** (lhs: Float, rhs: Float) -> Float {
+    // http://nshipster.com/swift-operators/
+    return pow(lhs, rhs)
+}
+
+// swiftlint:disable next identifier_name
+prefix operator √
+/// Square root of float.
+///
+/// - Parameter float: float value to find square root for
+/// - Returns: square root of given float.
+public prefix func √ (float: Float) -> Float {
+    // http://nshipster.com/swift-operators/
+    return sqrt(float)
+}
+
+// swiftlint:disable next identifier_name
+infix operator ±
+/// Tuple of plus-minus operation.
+///
+/// - Parameters:
+///   - lhs: number
+///   - rhs: number
+/// - Returns: tuple of plus-minus operation ( 2.5 ± 1.5 -> (4, 1)).
+public func ±<T: FloatingPoint> (lhs: T, rhs: T) -> (T, T) {
+    // http://nshipster.com/swift-operators/
+    return (lhs + rhs, lhs - rhs)
+}
+
+// swiftlint:disable next identifier_name
+prefix operator ±
+/// Tuple of plus-minus operation.
+///
+/// - Parameter int: number
+/// - Returns: tuple of plus-minus operation (± 2.5 -> (2.5, -2.5)).
+public prefix func ±<T: FloatingPoint> (number: T) -> (T, T) {
+    // http://nshipster.com/swift-operators/
+    return 0 ± number
+}
--- Classes/Swift/Int/Int+Normal.swift
+++ Classes/Swift/Int/Int+Normal.swift
@@ -11,8 +11,194 @@ import Foundation
 
 extension Int {
     
-    func stringValue() -> String? {
+    func stringValue() -> String {
         return "\(self)"
     }
     
+    /// Radian value of degree input.
+    public var degreesToRadians: Double {
+        return Double.pi * Double(self) / 180.0
+    }
+    
+    /// Degree value of radian input
+    public var radiansToDegrees: Double {
+        return Double(self) * 180 / Double.pi
+    }
+    
+    /// Array of digits of integer value.
+    /// 个位在右
+    public var digits: [Int] {
+        guard self != 0 else { return [0] }
+        var digits = [Int]()
+        var number = Swift.abs(self)
+        
+        while number != 0 {
+            let xNumber = number % 10
+            digits.append(xNumber)
+            number /= 10
+        }
+        
+        digits.reverse()
+        return digits
+    }
+
+    /// Number of digits of integer value.
+    /// 十进制位数
+    public var digitsCount: Int {
+        guard self != 0 else { return 1 }
+        let number = Double(Swift.abs(self))
+        return Int(log10(number) + 1)
+    }
+    
+    /// Random integer between two integer values.
+    ///
+    /// - Parameters:
+    ///   - min: minimum number to start random from.
+    ///   - max: maximum number random number end before.
+    /// - Returns: random double between two double values.
+    public static func random(between min: Int, and max: Int) -> Int {
+        return random(inRange: min...max)
+    }
+    
+    /// Random integer in a closed interval range.
+    ///
+    /// - Parameter range: closed interval range.
+    /// - Returns: random double in the given closed range.
+    public static func random(inRange range: ClosedRange<Int>) -> Int {
+        let delta = UInt32(range.upperBound - range.lowerBound + 1)
+        return range.lowerBound + Int(arc4random_uniform(delta))
+    }
+    
+    /// Check if given integer prime or not. 质数判断
+    /// Warning: Using big numbers can be computationally expensive!
+    /// - Returns: true or false depending on prime-ness
+    public func isPrime() -> Bool {
+        // To improve speed on latter loop :)
+        if self == 2 {
+            return true
+        }
+        
+        guard self > 1 && self % 2 != 0 else {
+            return false
+        }
+        // Explanation: It is enough to check numbers until
+        // the square root of that number. If you go up from N by one,
+        // other multiplier will go 1 down to get similar result
+        // (integer-wise operation) such way increases speed of operation
+        let base = Int(sqrt(Double(self)))
+        for int in Swift.stride(from: 3, through: base, by: 2) where self % int == 0 {
+            return false
+        }
+        return true
+    }
+    
+    /// Roman numeral string from integer (if applicable).
+    ///
+    ///10.romanNumeral() -> "X"
+    ///
+    /// - Returns: The roman numeral string.
+    public func romanNumeral() -> String? {
+        // https://gist.github.com/kumo/a8e1cb1f4b7cff1548c7
+        guard self > 0 else { // there is no roman numerals for 0 or negative numbers
+            return nil
+        }
+        let romanValues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        let arabicValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        
+        var romanValue = ""
+        var startingValue = self
+        
+        for (index, romanChar) in romanValues.enumerated() {
+            let arabicValue = arabicValues[index]
+            let div = startingValue / arabicValue
+            if div > 0 {
+                for _ in 0..<div {
+                    romanValue += romanChar
+                }
+                startingValue -= arabicValue * div
+            }
+        }
+        return romanValue
+    }
+    
+    /// Rounds to the closest multiple of n
+    /// 最小公倍数
+    public func roundToNearest(_ n: Int) -> Int {
+        return n == 0 ? self : Int(round(Double(self) / Double(n))) * n
+    }
 }
+
+// MARK: - Initializers
+public extension Int {
+    
+    /// Created a random integer between two integer values.
+    ///
+    /// - Parameters:
+    ///   - min: minimum number to start random from.
+    ///   - max: maximum number random number end before.
+    public init(randomBetween min: Int, and max: Int) {
+        self = Int.random(between: min, and: max)
+    }
+    
+    /// Create a random integer in a closed interval range.
+    ///
+    /// - Parameter range: closed interval range.
+    public init(randomInRange range: ClosedRange<Int>) {
+        self = Int.random(inRange: range)
+    }
+    
+}
+
+// MARK: - Operators
+
+precedencegroup PowerPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ** : PowerPrecedence
+/// Value of exponentiation.
+///
+/// - Parameters:
+///   - lhs: base integer.
+///   - rhs: exponent integer.
+/// - Returns: exponentiation result (example: 2 ** 3 = 8).
+public func ** (lhs: Int, rhs: Int) -> Double {
+    // http://nshipster.com/swift-operators/
+    return pow(Double(lhs), Double(rhs))
+}
+
+// swiftlint:disable next identifier_name
+prefix operator √
+/// Square root of integer.
+///
+/// - Parameter int: integer value to find square root for
+/// - Returns: square root of given integer.
+public prefix func √ (int: Int) -> Double {
+    // http://nshipster.com/swift-operators/
+    return sqrt(Double(int))
+}
+
+// swiftlint:disable next identifier_name
+infix operator ±
+/// Tuple of plus-minus operation.
+///
+/// - Parameters:
+///   - lhs: integer number.
+///   - rhs: integer number.
+/// - Returns: tuple of plus-minus operation (example: 2 ± 3 -> (5, -1)).
+public func ± (lhs: Int, rhs: Int) -> (Int, Int) {
+    // http://nshipster.com/swift-operators/
+    return (lhs + rhs, lhs - rhs)
+}
+
+// swiftlint:disable next identifier_name
+prefix operator ±
+/// Tuple of plus-minus operation.
+///
+/// - Parameter int: integer number
+/// - Returns: tuple of plus-minus operation (example: ± 2 -> (2, -2)).
+public prefix func ± (int: Int) -> (Int, Int) {
+    // http://nshipster.com/swift-operators/
+    return 0 ± int
+}
+
+
+
+
